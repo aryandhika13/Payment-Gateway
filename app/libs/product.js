29fcce659675aa0d963be359d319108cb6306276
Aryandhika Ibnu Raihan <aryandhika11mipa4@gmail.com>
2024-01-11 14:37:32 +0700

    Arya

---
+++ app/libs/product.js
@@ -0,0 +1,7 @@
+export const product = {
+    id: ~~(Math.random() * 100) + 1,
+    image: "/muka5.jpg",
+    name: "White Sneakers",
+    price: 219000,
+    description: "White Sneakers , its a Good product with a good quality. Size available from 36 to 44 ft",
+}
